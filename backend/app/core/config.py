cf0233a905d6115c44f4abb6525a98f47b720a59
JEAN-STEPH DEV <nguessanjeanstephane87@gmail.com>
2026-06-22 23:29:26 +0000

    Update DATABASE_URL for database connection

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     DEBUG: bool = False
 
     # ─── Database ─────────────────────────────────────────────────────────────
-    DATABASE_URL: str = "postgresql://api_syntax_ai_user:OWuybyBtmQZeMreJ6dYwZgAEwnyEXP7q@dpg-d8ss25vavr4c738f86u0-a/api_syntax_ai"
+    DATABASE_URL: str = "postgresql://api_syntax_ai_sql_user:c8aZ3xSvygj1wEFhbpThyBzrPbmaZRmu@dpg-d8ss96smmk8c73dn87a0-a/api_syntax_ai_sql"
 
     # ─── Redis ────────────────────────────────────────────────────────────────
     REDIS_URL: str = "redis://red-d8ss3su7r5hc73e7563g:6379"
